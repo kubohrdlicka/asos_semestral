043242432f77ed3d42559b4841494edbf3d26395
kubohrdlicka <jakubhr124@gmail.com>
2024-11-10 21:18:17 +0100

    feat: added basic views structure for future components

--- changelog.md
+++ changelog.md
@@ -4,3 +4,4 @@ Project progress:
 
 - _[26.09.2024] Jakub Hrdlička:_ created github repo
 - _[26.10.2024] Šimon Drlička:_ setup backend application for Rest api
+- _[10.11.2024] Jakub Hrdlička:_ initiated frontend app, created basic routes structure
--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -1,6 +1,7 @@
 import NotFound from '@/views/NotFoundView.vue'
 import { RouteRecordRaw } from 'vue-router'
 import PlainLayout from '@/layout/PlainLayout.vue'
+import SimpleLayout from '@/layout/SimpleLayout.vue'
 
 export const routes: RouteRecordRaw[] = [
   {
@@ -8,9 +9,55 @@ export const routes: RouteRecordRaw[] = [
     path: '/',
     component: () => import('@/views/LandingView.vue'),
     meta: {
-      title: 'Dashboard',
+      title: 'Landing Page',
       layout: PlainLayout
     }
+  },
+  {
+    name: 'login',
+    path: '/login',
+    component: () => import('@/views/LoginView.vue'),
+    meta: {
+      title: 'Login Page',
+      layout: PlainLayout
+    }
+  },
+  {
+    name: 'register',
+    path: '/register',
+    component: () => import('@/views/RegisterView.vue'),
+    meta: {
+      title: 'Register Page',
+      layout: PlainLayout
+    }
+  },
+  {
+    name: 'account',
+    path: '/account',
+    component: () => import('@/views/AccountView.vue'),
+    meta: {
+      title: 'Account Page',
+      layout: SimpleLayout
+    }
+  },
+  {
+    name: 'taskboard',
+    path: '/tasks',
+    component: () => import('@/views/TaskboardView.vue'),
+    meta: {
+      title: 'Taskboard Page',
+      layout: SimpleLayout
+    },
+    children: [
+      {
+        path: 'new',
+        component: () => import('@/views/TaskCreateView.vue')
+      },
+      {
+        path: ':id',
+        component: () => import('@/views/TaskDetailView.vue')
+      }
+    ]
   },
   { path: '/:path(.*)', component: NotFound }
 ]
