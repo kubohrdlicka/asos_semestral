370960361ab3d88d593d499d272faf1ec897f0c5
kubohrdlicka <jakubhr124@gmail.com>
2024-11-12 21:20:39 +0100

    feat: some FE structure skeletons

---
+++ frontend/src/components/CalendarWidget.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <div>Calendar</div>
+    <div>~Today, tomorrow, in next week</div>
+  </div>
+</template>
---
+++ frontend/src/components/TasksListWidget.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>Tasks - {{ props.widgetType }}</div>
+    <div>list...</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { PropType } from 'vue';
+import { TaskWidgetType } from '@/types/task';
+
+const props = defineProps({
+  widgetType: {
+    type: String as PropType<TaskWidgetType>,
+    required: true,
+  },
+})
+</script>
