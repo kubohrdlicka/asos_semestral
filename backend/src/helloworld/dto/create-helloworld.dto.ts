9d49f748449ffcc471da8cd2c664735fa25e1ff7
MatKollar <matej.kollar3@gmail.com>
2024-11-23 18:55:25 +0100

    remove unused controller

--- backend/src/helloworld/dto/create-helloworld.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
-
-export class CreateHelloworldDto {
-  @ApiProperty({ description: 'Name description' })
-  @IsString({ message: 'Name must be a string' })
-  name: string;
-}
--- backend/src/helloworld/dto/update-helloworld.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateHelloworldDto } from './create-helloworld.dto';
-import { ApiProperty } from '@nestjs/swagger';
-
-export class UpdateHelloworldDto extends PartialType(CreateHelloworldDto) {
-  @ApiProperty()
-  name: string;
-}
