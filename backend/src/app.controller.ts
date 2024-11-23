82004085aef71a208377f9e01f8a9afc61aafcc4
MatKollar <matej.kollar3@gmail.com>
2024-11-23 12:12:20 +0100

    fix dependencies

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -10,22 +10,4 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  // @Public()
-  // @UseGuards(LocalAuthGuard)
-  // @Post('auth/login')
-  // async login(@Request() req) {
-  //   return await this.authService.login(req.user);
-  // }
-
-  // @Public()
-  // @Post('auth/register')
-  // async register(@Body() user: CreateUserDto) {
-  //   return await this.usersService.create(user);
-  // }
-
-  // @UseGuards(JwtAuthGuard)
-  // @Get('auth/me')
-  // async getProfile(@LoggedInUser() loggedInUser: User) {
-  //   return await this.usersService.findOne(loggedInUser.id);
-  // }
 }
